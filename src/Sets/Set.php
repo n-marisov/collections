f817dd77bbdaad409b7e96fdc4cc71669bdcf1e0
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-04-11 18:46:37 +0300

    Изменено название класса ArrayList::class на MutableList::class, добавлена ссылка на composer, примеры вынесены в отдельные файлы.

---
+++ src/Sets/Set.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Maris\Collection\Sets;
+
+use Maris\Collection\AbstractCollection;
+use Maris\Collection\Interfaces\ListInterface;
+use Maris\Collection\Interfaces\MapInterface;
+use Maris\Collection\Interfaces\SetInterface;
+use Maris\Collection\Lists\MutableList;
+use Maris\Collection\Maps\Map;
+
+/**
+ * Лист в котором хранятся только уникальные значения
+ * Ключи не обеспечивают необходимой точности и должны
+ * игнорироваться.
+ * Может быть распакован оператором spread.
+ */
+class Set extends AbstractCollection implements SetInterface
+{
+    public function add(...$values): static
+    {
+        foreach ($values as $value)
+            if(!in_array($value,$this->values,true))
+                $this->values[] = $value;
+        return $this;
+    }
+
+    public function has( ...$values ): bool
+    {
+        if(empty($values)) return false;
+        foreach ($values as $value)
+            if(!in_array($value,$this->values,true))
+                return false;
+        return true;
+    }
+
+    public function delete(...$values): static
+    {
+        foreach ($values as $key => $value)
+            if($this->has($value)){
+                $this->count--;
+                unset($this->values[ $key ]);
+            }
+        $this->values = array_values($this->values);
+        return $this;
+    }
+
+    public function toMap(): MapInterface
+    {
+        $map = new Map();
+        foreach ($this->values as $key => $value)
+            $map->set( $key, $value );
+        return $map;
+    }
+
+    public function toList(): ListInterface
+    {
+        return (new MutableList())->push();
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        if(!$this->has($value))
+            $this->values[] = $value;
+    }
+
+
+}
